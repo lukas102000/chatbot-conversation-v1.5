811158332a64ba1ecf534ea2f6ab7550459a8eb3
lukas102000 <lucasveraz102000@gmail.com>
2022-12-14 20:11:36 +0100

    commit

--- asset/js/bot-mini.js
+++ asset/js/bot-mini.js
@@ -1,14 +1,8 @@
-var getInput = document.getElementById("userInput")
-getInput.addEventListener("keyup",(event)=>{
-    if(event.keyCode === 13){
-        send_message()
-    }
-})
-
+var num = 0
 const send_message = ()=>{
     var getInput = document.getElementById("userInput").value 
 
-    var filter = getInput
+    var filter = getInput.toLowerCase()
     get_conversation(filter)
     
     document.getElementById("userInput").value = ""
@@ -19,14 +13,11 @@ function demoAnswer(){
     bot_div.className = "bot_div"
 
     var custon_res = [
-        "Olá seja muito ben-vindo! E será um prazer ter uma conversa contigo, isto ajudará-nos a melhorar a qualidades"+
-        " das nossas pesquisas online.",
-        " NOTA: Algumas perguntas serão repetitiva, para ajudar o flúxo da conversa",
-        " Podemos continuar?"
+        "Olá seja muito ben-vindo! Gostaria de fazer-te algumas perguntas, podemos começar?"
     ]
     var box_div = document.createElement("div")
     box_div.className = "box_div"
-    var logo = "<img src='./asset/img/Chatbot-Logo-Modern-bot-logo-Graphics-14298242-1.jpg'  height='50px'>"
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
     
     
     box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+ custon_res +"</div>"
@@ -34,7 +25,7 @@ function demoAnswer(){
     btn_sender("sim podemos")
     btn_sender("não podemos")
 }
-const get_conversation = (_user,bot)=>{
+const get_conversation = (user,bot)=>{
     
     var box = document.querySelector(".containt-message")
     var div = document.createElement("div")
@@ -42,42 +33,78 @@ const get_conversation = (_user,bot)=>{
     div.className = "boxSms"
     bot_div.className = "bot_div"
 
-    var bot_response = conversation(_user)
-    var bot_quest = question()
+    var bot_response = conversation(user.toLowerCase())
     var box_div = document.createElement("div")
     box_div.className = "box_div"
-    var logo = "<img src='./asset/img/Chatbot-Logo-Modern-bot-logo-Graphics-14298242-1.jpg'  height='50px'>"
-    
-    
-    
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
+    var listen = [
+        "",
+        "Qual é o seu nome?",
+        "És de que país?",
+        "e qual é a tua cidade?",
+        "bem "+localStorage.getItem("name")+", tens filhos?",
+        "muito bem "+localStorage.getItem("name")+", Tu gostas de um animal doméstico?",
+        "Qual é a tua situação actual?, trabalhas ou estudante!",        
+        "Quais são os serviços de táxis há na cidade de "+localStorage.getItem("cidade")+" ?",
+        "Gostas de séries? "+localStorage.getItem("name"),
+        "incrível "+localStorage.getItem("name")+". Qual é a tua séries favorita? "+localStorage.getItem("name"),
+        "Qual é o serviço TV que usas, NetFlix, Youtube? Canal+ ou outros? Escreve-as!",
+        "Será que, tu frequentou um shoping este més? "+localStorage.getItem("name"),
+        "Qual do shoping tu me encomendaria? "+localStorage.getItem("name"),
+        "Porque, tu me encomendaria o shoping "+localStorage.getItem("shoping")+"? "+localStorage.getItem("name"),
+        "Já usou um cartão crédito?",
+        ""+localStorage.getItem("name")+" Neste més, comprou um ítens online?",
+        "Quantos gastou este més fazendo compras?",
+        "Quem faz as compras em sua casa? "+localStorage.getItem("name"),
+        "Só falta mais um pouco, para terminamos. Está bem? "+localStorage.getItem("name"),
+        
+        "Qual é o serviço de táxi, que mais usas? "+localStorage.getItem("sTaxi"),
+        "Quantos gastou este més usando o "+localStorage.getItem("sTaxi"),
+        "Quais dos serviços de táxis que realizou uma publicidade este més?"+ localStorage.getItem("sTaxi")+" ?",
+
+        "Se um amigo quer chegar na cidade de "+localStorage.getItem("país")+", em que hótel ficaria? de tua preferência?",
+
+        "quantos gastas para pagamentos mensal da "+localStorage.getItem("sTV"),
+        "Quais dos canais mais adorada por ti na "+localStorage.getItem("sTV"),
+        "Qual das personagens que gostas na série de "+localStorage.getItem("serie"),
+        
+        "Como é conseguir um emprego na cidade de "+localStorage.getItem("cidade"),
+        "Quais das áreas que mais emprega as pessoas na cidade de "+localStorage.getItem("cidade"),
+        
+        "gostaria muito saber mais sobre a "+localStorage.getItem("sTV")+" Mas, infelizmente a nossa pesquisa termina por aqui "+
+        "agradeço pela tua paciência! "+localStorage.getItem("name")+
+        " e para a tua segurança estas conversas não são guardadas, podes atualizar a página e tudo sumirá! obrigada..",
+        "não há mais assunto no momento "+localStorage.getItem("name")
+    ]
     
     box.append(div)
-    if(_user == ""){
+    if(user == ""){
         div.style.display = "none"
         emoji2() 
+        box_div.innerHTML = logo + "<br>"+"<img src = './asset/img/emoji100(2).png' height='50px';>"
+        box.append(box_div)
     }else{
-            div.innerHTML = _user
+            div.innerHTML = user
+            
             if(bot_response == undefined){
-                box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+ bot_quest+ "</div>"
-                box.append(box_div)
-                bot_learning(bot_quest, _user)
-            }else{
-                if(bot_response == "está bem, estás preparada para começar?"){
-                    box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+ bot_response+ "</div>"
-                    box.append(div)
+
+                if(num <= listen.length -1){
+                    var ls = listen[num += 1]
+                    box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+ls+ "</div>"
                     box.append(box_div)
-                    btn_sender("sim estou prepada")
-                    btn_sender("não estou preparada")
+                    console.log(ls)
+                    bot_learning(ls, user)
                 }else{
-                    box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+ bot_response+ "</div>"
-                    box.append(div)
-                    box.append(box_div)   
-                }
-                if(bot_response == "então vamos começar"){
-                    box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+ bot_quest+ "</div>"
+                    box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >não há mais assunto no momento "+
+                    localStorage.getItem("name")+"</div>"
                     box.append(box_div)
                 }
                 
+                
+            }else{
+                box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+ bot_response+ "</div>"
+                box.append(div)
+                box.append(box_div)                 
         }
     }
     document.querySelector("#like").setAttribute("src", "./asset/img/emoji100(2).png")
@@ -105,10 +132,6 @@ function conversation (conversation){
 // bot learning new response
 const bot_learning = (quest, answer)=>{
     return bot_learning_response(quest, answer)
-}
-// importando as questões aleatorios
-const question = ()=>{
-    return quetions()
 }
 
 function emoji1(){
@@ -159,7 +182,6 @@ function emoji6(){
     div.innerHTML = "<img src = './asset/img/emoji3100.png' height='50px';>"
     box.append(div)
 }
-
 function change_icon(){
     var img ="./asset/img/sen-icon100.png"
     document.querySelector("#like").setAttribute("src", img)
--- chattetbot/chatbot.js
+++ chattetbot/chatbot.js
@@ -1,3 +1,5 @@
+var num = 0
+
 const conversation_demo = (conversation)=>{
     const listen = {
         "oi":"olá",
@@ -5,7 +7,7 @@ const conversation_demo = (conversation)=>{
         "como estás?": "estou bem e tu?",
         "como estás": "estou bem e tu?",
         "como vás": "vou bem e tu?",
-        "Tudo bem?": "tudo e contigo?",
+        "tudo bem?": "tudo e contigo?",
         "tambm estou bem": "o dia como foi?",
         "também bem": "o dia como foi?",
         "também": "o dia como foi?",
@@ -18,7 +20,6 @@ const conversation_demo = (conversation)=>{
         "que horas são?": "não sei, veja ao seu celular, por favor",
         "e os seus": "normal",
         "que situação?": "situação, financeira, trabalho, etc... tu estudas?, ou trabalhas",
-        "não":"está bem",
         "sim estudo": "qual é o teu académico?",
         "sim trabalho": "gostas do teu trabalho?",
         "sim gosto do meu trabalho": "que bom",
@@ -29,69 +30,66 @@ const conversation_demo = (conversation)=>{
     }
     return listen[conversation]
 }
-const quetions = (op)=>{
-    listen = [
-        "Como tem sido os teus dias?",
-        "Como está correndo as tuas actividades?",
-        "Qual é a tua situação actual?",
-        "Tu tens filhos?",
-        "Tu gostas de um animal doméstico?",
-        "Como se chama o seu animal doméstico?",
-        "Que cidade tu és?",
-        "Quais são os serviços de táxis há em tua cidade?",
-        "Já usou um cartão crédito esse més?",
-        "Quantos já gastou este més?",
-        "Neste més. tu comprou um ítens online?",
-        "Qual é o serviço TV que usas, NetFlix, Youtube? Canal +? Escreve-a",
-        "Será que, tu frequentou um shoping este més?",
-        "Qual do shoping tu me encomendaria?",
-        "Qual é o serviço de táxi, que mais usas? "+localStorage.getItem("sTaxi"),
-        "Estudas?",
-        "Trabalhas?",
-        "Só falta mais um pouco, para terminamos...",
-        "És de que país?",
-        "Qual é a tua séries favorita?",
-        "Se um amigo quer chegar em "+localStorage.getItem("país")+", em que hótel ficaria? de tua preferência?",
-        "quantos gastas para pagamentos mensal da "+localStorage.getItem("sTV"),
-        "Quais dos serviços de táxis que realizou uma publicidade este més?",
-        "Quantos gastou este més usando o "+localStorage.getItem("sTaxi"),
-        "Como é conseguir um emprego em "+localStorage.getItem("cidade"),
-        "Quais das áreas que mais emprega as pessoas em "+localStorage.getItem("cidade"),
-        "Quais dos canais mais adorada por ti na "+localStorage.getItem("sTV"),
-        "Qual das personagens que gostas na série de "+localStorage.getItem("serie"),
-        "Porque, tu me encomendaria o shoping "+localStorage.getItem("shoping")+"?"
-    ]
-    var random = Math.floor(Math.random() * listen.length)
-    var result = listen[random]
-    return result
-}
+
 const bot_learning_response = (quest, _user)=>{
-    if(quest == "Que cidade tu és?"){
+    if(quest == "e qual é a tua cidade?"){
         save_cidade()
     }
     if(quest == "És de que país?"){
         save_pais()        
     }
-    if(quest == "Qual é o serviço TV que usas, NetFlix, Youtube? Canal +? Escreve-a"){
+    if(quest == "Qual é o serviço TV que usas, NetFlix, Youtube? Canal+ ou outros? Escreve-as!" ){
         save_STV()        
     }
-    if(quest == "Quais são os serviços de táxis há em tua cidade?"){
+    if(quest == "Quais são os serviços de táxis há na cidade de "+localStorage.getItem("cidade")+" ?" ){
         save_sTaxi()
     }
-    if(quest == "Qual é a tua séries favorita?"){
+    if(quest == "incrível "+localStorage.getItem("name")+". Qual é a tua séries favorita? "+localStorage.getItem("name") ){
         save_series()
     }
-    if(quest == "Qual do shoping tu me encomendaria?"){
+    if(quest == "Qual do shoping tu me encomendaria? "+localStorage.getItem("name") ){
         save_shoping()
     }
+    if(quest == "Qual é o seu nome?"){
+        save_name()
+    }
 }
+function save_name(){
+    var box = document.querySelector(".containt-message")
+    var inp = document.createElement("input")
+    var box_div = document.createElement("div")
+    box_div.className = "box_div"
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
+    
+    inp.setAttribute("id", "idname")
+    inp.className = "inp-res"
+    var btn = document.createElement("button")
 
+    btn.className = "btn-res"
+    btn.innerHTML = "Enviar"
+
+    document.getElementById("sendBtn").style.display = "none"
+        btn.addEventListener("click",()=>{
+            var getter = document.getElementById("idname").value
+            localStorage.setItem("name", getter)
+            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+getter+" Podemos continuar?</div>"
+            document.getElementById("sendBtn").style.display = "flex"
+            document.querySelector(".btn-res").style.display = "none"
+            
+            box.append(box_div) 
+            btn_senders("Sim podemos continuar")
+            
+        })
+        box.append(inp)
+        box.append(btn)
+
+}
 function save_pais(){
     var box = document.querySelector(".containt-message")
     var inp = document.createElement("input")
     var box_div = document.createElement("div")
     box_div.className = "box_div"
-    var logo = "<img src='./asset/img/Chatbot-Logo-Modern-bot-logo-Graphics-14298242-1.jpg'  height='50px'>"
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
     
     inp.setAttribute("id", "idpais")
     inp.className = "inp-res"
@@ -104,18 +102,16 @@ function save_pais(){
         btn.addEventListener("click",()=>{
             var getter = document.getElementById("idpais").value
             localStorage.setItem("país", getter)
-            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+getter+"Podemos continuar?</div>"
+            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+localStorage.getItem("name")+" Podemos continuar?</div>"
             document.getElementById("sendBtn").style.display = "flex"
             document.querySelector(".btn-res").style.display = "none"
             
             box.append(box_div) 
             btn_senders("Sim podemos continuar")
-            var getter = document.getElementById("inpgeter").value = ""
+            
         })
         box.append(inp)
         box.append(btn)
-        
-
 
 }
 function save_cidade(){
@@ -123,7 +119,7 @@ function save_cidade(){
     var inp = document.createElement("input")
     var box_div = document.createElement("div")
     box_div.className = "box_div"
-    var logo = "<img src='./asset/img/Chatbot-Logo-Modern-bot-logo-Graphics-14298242-1.jpg'  height='50px'>"
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
     inp.setAttribute("id", "idcidade")
     inp.className = "inp-res"
     var btn = document.createElement("button")
@@ -136,10 +132,10 @@ function save_cidade(){
             localStorage.setItem("cidade", getter)
             document.getElementById("sendBtn").style.display = "flex"
             document.querySelector(".btn-res").style.display = "none"          
-            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >Podemos continuar?</div>"
+            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+localStorage.getItem("name")+" Podemos continuar?</div>"
             box.append(box_div)
             btn_senders("Sim podemos continuar")
-            var getter = document.getElementById("inpgeter").value = ""
+            
         })
         box.append(inp)
         box.append(btn)
@@ -149,7 +145,7 @@ function save_sTaxi(){
     var inp = document.createElement("input")
     var box_div = document.createElement("div")
     box_div.className = "box_div"
-    var logo = "<img src='./asset/img/Chatbot-Logo-Modern-bot-logo-Graphics-14298242-1.jpg'  height='50px'>"
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
     inp.setAttribute("id", "idTaxi")
     inp.className = "inp-res"
     var btn = document.createElement("button")
@@ -161,10 +157,10 @@ function save_sTaxi(){
             localStorage.setItem("sTaxi", getter)
             document.getElementById("sendBtn").style.display = "flex"
             document.querySelector(".btn-res").style.display = "none"
-            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >Podemos continuar?</div>"
+            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+localStorage.getItem("name")+" Podemos continuar?</div>"
             box.append(box_div) 
             btn_senders("Sim podemos continuar")
-            var getter = document.getElementById("inpgeter").value = ""
+            
         })
         box.append(inp)
         box.append(btn)
@@ -174,7 +170,7 @@ function save_STV(){
     var inp = document.createElement("input")
     var box_div = document.createElement("div")
     box_div.className = "box_div"
-    var logo = "<img src='./asset/img/Chatbot-Logo-Modern-bot-logo-Graphics-14298242-1.jpg'  height='50px'>"
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
     inp.setAttribute("id", "idsTV")
     inp.className = "inp-res"
     var btn = document.createElement("button")
@@ -186,10 +182,10 @@ function save_STV(){
             localStorage.setItem("sTV", getter)
             document.getElementById("sendBtn").style.display = "flex"
             document.querySelector(".btn-res").style.display = "none"
-            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >Podemos continuar?</div>"
+            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+localStorage.getItem("name")+" Podemos continuar?</div>"
             box.append(box_div) 
             btn_senders("Sim podemos continuar")
-            var getter = document.getElementById("inpgeter").value = ""
+            
         })
         box.append(inp)
         box.append(btn)
@@ -199,7 +195,7 @@ function save_series(){
     var inp = document.createElement("input")
     var box_div = document.createElement("div")
     box_div.className = "box_div"
-    var logo = "<img src='./asset/img/Chatbot-Logo-Modern-bot-logo-Graphics-14298242-1.jpg'  height='50px'>"
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
     inp.setAttribute("id", "idseries")
     inp.className = "inp-res"
     var btn = document.createElement("button")
@@ -211,10 +207,10 @@ function save_series(){
             localStorage.setItem("serie",getter)
             document.getElementById("sendBtn").style.display = "flex"
             document.querySelector(".btn-res").style.display = "none"
-            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >Podemos continuar?</div>"
+            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+localStorage.getItem("name")+" Podemos continuar?</div>"
             box.append(box_div) 
             btn_senders("Sim podemos continuar")
-            var getter = document.getElementById("inpgeter").value = ""
+            
         })
         box.append(inp)
         box.append(btn)
@@ -224,7 +220,7 @@ function save_shoping(){
     var inp = document.createElement("input")
     var box_div = document.createElement("div")
     box_div.className = "box_div"
-    var logo = "<img src='./asset/img/Chatbot-Logo-Modern-bot-logo-Graphics-14298242-1.jpg'  height='50px'>"
+    var logo = "<img src='./asset/img/log.png'  height='50px'>"
     inp.setAttribute("id", "idshoping")
     inp.className = "inp-res"
     var btn = document.createElement("button")
@@ -236,10 +232,10 @@ function save_shoping(){
             localStorage.setItem("shoping",getter)
             document.getElementById("sendBtn").style.display = "flex"
             document.querySelector(".btn-res").style.display = "none"
-            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >Podemos continuar?</div>"
+            box_div.innerHTML = logo + "<br>"+ "<div class = 'bot_div' >"+localStorage.getItem("name")+" Podemos continuar?</div>"
             box.append(box_div) 
             btn_senders("Sim podemos continuar")
-            var getter = document.getElementById("inpgeter").value = ""
+            
         })
         box.append(inp)
         box.append(btn)
